dfeb89950ba764ffd8075a97489affc4e73ea2a9
Pradyumna Wagholikar <pradyumnawagholikar@Pradyumnas-MacBook-Air.local>
2024-04-01 00:20:35 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const dotenv=require("dotenv");
+const {chats} = require("./data/data");
+const connectDB = require('./config/db');
+dotenv.config();
+const app = express();
+
+connectDB();
+
+app.get('/', (req, res) => {
+    res.send("API running successfully");
+}); 
+
+app.get("/api/chat", (req, res) => {
+    res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+    //console.log(req.params.id);
+    const singleChat=chats.find((c) => c._id === req.params.id);
+    res.send(singleChat);
+});
+
+const PORT=process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}` );
+});
